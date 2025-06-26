a34b64c42d02723583a5749693eeef057736e25c
Duscov <dmduscov@gmail.com>
2025-06-26 12:56:36 +0200

    added Service layer for Product

--- src/main/java/app/repositories/ProductRepositoryMap.java
+++ src/main/java/app/repositories/ProductRepositoryMap.java
@@ -1,7 +1,6 @@
 package app.repositories;
 
 import app.domain.Product;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,14 +9,14 @@ import java.util.stream.Collectors;
 
 public class ProductRepositoryMap implements ProductRepository{
 
-    private Map<Integer, Product> database = new HashMap<>();
+    private Map<Long, Product> database = new HashMap<>();
 
     private long currentId = 0;
 
     @Override
     public Product save(Product product) {
         product.setId(++currentId);
-        database.put((int) currentId,product);
+        database.put(currentId,product);
         return product;
     }
 
@@ -47,13 +46,33 @@ public class ProductRepositoryMap implements ProductRepository{
         Long id = product.getId();
         double newPrice = product.getPrice();
         String newName = product.getName();
+        boolean newActive = product.isActive();
 
         Product oldProduct = findById(id);
 
         if(oldProduct != null) {
             oldProduct.setName(newName);
             oldProduct.setPrice(newPrice);
+            oldProduct.setActive(newActive);
         }
         return oldProduct;
+    }
+
+    public static void main(String[] args) {
+        ProductRepository repository = new ProductRepositoryMap();
+        System.out.println(repository.save(new Product(true, "Coffee", 3)));
+        //TODO check 0 field name
+        System.out.println(repository.save(new Product(false, "Baget", 4)));
+        System.out.println(repository.findAll());
+        System.out.println(repository.findById(2L));
+        System.out.println("====================Delete================");
+        repository.deleteById(1L);
+        System.out.println(repository.findById(1L));
+        System.out.println("====================Update================");
+        Product newproduct = new Product(true, "Baguette", 7);
+        newproduct.setId(2L);
+        System.out.println(repository.updateById(newproduct));
+
+
     }
 }
